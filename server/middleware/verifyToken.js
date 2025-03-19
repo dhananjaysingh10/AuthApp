84a7f5110598aebd52958bf20d3d8f148d0446df
dhananjaysingh10 <itsdhananjaysingh04@gmail.com>
2025-03-19 15:09:28 +0530

    third

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-	const refreshToken = req.cookies.refToken;
-	const token = req.cookies.authToken;
+	const token = req.cookies.refToken;
+	const accessToken = req.cookies.authToken;
 	if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
