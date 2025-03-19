cb09f417adc1a3e1bc08d4c78df21b5adcc596e4
dhananjaysingh10 <itsdhananjaysingh04@gmail.com>
2025-03-19 15:13:43 +0530

    added ref token var

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -95,8 +95,14 @@ export const checkAuth = async (req, res) => {
 
 export const refreshToken = async (req, res) => {
     try {
-        
-    } catch (error) {
-        
-    }
+		const user = await User.findById(req.userId).select("-password");
+		if (!user) {
+			return res.status(400).json({ success: false, message: "User not found" });
+		}
+
+		res.status(200).json({ success: true, user });
+	} catch (error) {
+		console.log("Error in checkAuth ", error);
+		res.status(400).json({ success: false, message: error.message });
+	}
 };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { checkAuth, logout, login, signup, refreshToken} from "../controllers/auth.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import { verifyRefToken } from "../middleware/verifyRefToken.js";
 const router = express.Router();
 
 router.post('/register', signup)
 router.post('/login', login)
 router.post('/logout', logout)
-router.post('/token/refresh', refreshToken)
+router.post('/token/refresh', verifyRefToken, refreshToken)
 router.get("/check-auth", verifyToken, checkAuth)
 
 
